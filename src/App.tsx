22167cdf2cffcafa75ad95b5c2a82f9831cebc9d
Laith <laith.hilal@appliedtechnology.se>
2023-01-19 13:49:12 +0100

    added some info

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Post from './post';
 import BlogSection from './components/Blogsection';
 import Header from './components/Header';
+import Info from './components/Info';
 
 const App: React.FC = () => {
     const [posts, setPosts] = useState<Post[]>([]);
@@ -18,6 +19,7 @@ const App: React.FC = () => {
       return (
       <div className='main-container'>
         <Header />
+        <Info />
           {selectedTags.map( (tag, i) => (
               <BlogSection key={i} tag={tag} posts={posts} />
           ))}
---
+++ src/components/Info.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Info.css'
+
+const Info = () => {
+  return (
+    <div className='info'>
+        <h3>Pick a Tag to get their Posts:</h3>
+    </div>
+  )
+}
+
+export default Info
